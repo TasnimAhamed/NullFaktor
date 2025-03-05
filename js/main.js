1cfd0314dac4eb511db9ff1453a8e0704eced057
TasnimAhamed <tasnim.ahamed.20@gmail.com>
2025-03-05 21:57:37 +0600

    mobile menu is replace with sidebar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+    const navbarToggler = document.querySelector("#navbar-toggler");
+    const navbarContent = document.querySelector("#navbar-content");
+    const navbarCloser = document.querySelector("#mobile__navigation-closer");
+    navbarToggler.addEventListener("click", function(){
+        navbarContent.classList.toggle("active");
+    })
+    navbarCloser.addEventListener("click", function(){
+        navbarContent.classList.toggle("active");
+    })
 })
