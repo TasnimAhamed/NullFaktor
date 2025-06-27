2ac534c48ae5e110ba154aae7a0843753cb50fd9
TasnimAhamed <tasnim.ahamed.20@gmail.com>
2025-06-28 00:27:45 +0600

    Added slider on why us section

--- js/main.js
+++ js/main.js
@@ -8,4 +8,29 @@ document.addEventListener("DOMContentLoaded", function(){
     navbarCloser.addEventListener("click", function(){
         navbarContent.classList.toggle("active");
     })
+
+    const featureSliderInstance = new Splide('.why__us-slider', {
+        type   : 'loop',
+        perPage: 3,
+        perMove: 1,
+        gap    : '24px',
+        drag   : false,
+        arrows : false,
+        pagination: false,
+        breakpoints: {
+            1024: {
+                perPage: 2,
+                padding: '0px',
+                drag: true,
+            },
+            640: {
+                perPage: 1,
+                padding: '8%',
+                gap: '16px',
+                focus     : 'center',
+                start: 1,
+                drag: true,
+            },
+        },
+    }).mount();
 })
